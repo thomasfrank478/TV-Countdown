d9fdf2b9b32cf626f1dda62bba4b5869c89a759e
Thomas Frank <ostlyng.t@gmail.com>
2021-04-21 17:38:56 +0200

    Initial Commit

---
+++ TV Countdown/TV_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TV_CountdownApp.swift
+//  TV Countdown
+//
+//  Created by Thomas Østlyng on 21/4/21.
+//
+
+import SwiftUI
+
+@main
+struct TV_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
